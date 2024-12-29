834bdf15cc40feb16cfdbf4d9fb3ea29806b12e8
Quan Giap <minhquangiapnguyen@gmail.com>
2024-12-28 17:48:36 -0800

    add new rule for schema

--- src/routers/Image/Image.ts
+++ src/routers/Image/Image.ts
@@ -8,12 +8,12 @@ import fileUpload from 'express-fileupload';
 import prisma from '../../tools/PrismaSingleton';
 import { createErrRes } from '../../tools/ResTool';
 import { UserType } from '../../type/Type';
-const achievement_route = express.Router();
+const image_route = express.Router();
 
 /**
- * Upload image to cloud and insert id to achievement data
+ * Upload image to cloud and insert id to data table base on Image Type
  */
-achievement_route.post('/',verifyToken,
+image_route.post('/',verifyToken,
     uploadManager,
     checkValidImgMiddleware,
     checkValidJsonMiddleware
@@ -27,120 +27,8 @@ achievement_route.post('/',verifyToken,
         if(err_message.error){
             return createErrRes({...err_message,res});
         }
-        // const user_input = parsed_data[0];
-        // //check if user is owned portfolio
-        // const achievement = await prisma.achievement.findFirst({
-        //     where:{
-        //         id:user_input.achievement_id,
-        //     },
-        //     select:{
-        //         portfolioData:{
-        //             select:{
-        //                 user_id:true,
-        //             }
-        //         }
-        //     }
-        // })
-        // if(!achievement){
-        //     return createErrRes({error:'Achievement data not found',res,status_code:404})
-        // }
-        // if(user.id !== achievement.portfolioData.user_id){
-        //     return createErrRes({error:'Forbidden',res,status_code:403})
-        // }
-
-        // let imgs:fileUpload.UploadedFile[] = [];
-        // //files.imgs exist since checked
-        // if(Array.isArray(files.imgs)){
-        //     imgs = files.imgs;
-        // }else{
-        //     imgs.push(files.imgs);
-        // }
-        // const idsPromise = imgs.map(file=>uploadImgToStorage(file));
-        // const ids = await Promise.all(idsPromise);
-        // await prisma.achievement.update({
-        //     where:{
-        //         id:user_input.achievement_id,
-        //     },
-        //     data:{
-        //         img_ids:{
-        //             push:ids,
-        //         }
-        //     }
-        // })
+       const user_input = parsed_data[0];
         return res.json(`Insert image success`).status(201); 
 })
 
-/**
- * Update image and upload new image to cloud (if files exist) and update id to achievement data
- */
-achievement_route.patch('/img',verifyToken,
-    uploadManager,
-    checkValidImgMiddleware,
-    checkValidJsonMiddleware
-    ,async (req,res)=>{
-        //files exist since checked
-        const files = req.files as fileUpload.FileArray 
-        //req.body.user from verifyToken
-        const user = req.body.user as UserType;
-        //check user input
-        const {parsed_data,err_message} = checkValidInput([post_img_schema],[files.json]);
-        if(err_message.error){
-            return createErrRes({...err_message,res});
-        }
-        const user_input = parsed_data[0];
-        //check if user is owned portfolio
-        //get imgs ids to know which one should delete or not exist
-        // const achievement = await prisma.achievement.findFirst({
-        //     where:{
-        //         id:user_input.achievement_id,
-        //     },
-        //     select:{
-        //         portfolioData:{
-        //             select:{
-        //                 user_id:true,
-        //             }
-        //         }
-        //     }
-        // })
-        // if(!achievement){
-        //     return createErrRes({error:'Achievement data not found',res,status_code:404})
-        // }
-        // if(user.id !== achievement.portfolioData.user_id){
-        //     return createErrRes({error:'Forbidden',res,status_code:403})
-        // }
-
-        // let imgs:fileUpload.UploadedFile[] = [];
-        // //files.imgs exist since checked
-        // if(Array.isArray(files.imgs)){
-        //     imgs = files.imgs;
-        // }else{
-        //     imgs.push(files.imgs);
-        // }
-        // const idsPromise = imgs.map(file=>uploadImgToStorage(file));
-        // const ids = await Promise.all(idsPromise);
-        // await prisma.achievement.update({
-        //     where:{
-        //         id:user_input.achievement_id,
-        //     },
-        //     data:{
-        //         img_ids:{
-        //             push:ids,
-        //         }
-        //     }
-        // })
-        return res.json(`Insert image success`).status(201); 
-})
-
-/**
- * Create new achievement 
- */
-achievement_route.post('/',verifyToken,(req,res)=>{
-    //create new about
-})
-achievement_route.patch('/',verifyToken,(req,res)=>{
-    //update about
-})
-achievement_route.get('/:user_id/',(req,res)=>{
-    //get about from user_id
-})
-export default achievement_route;
+export default image_route;
--- src/routers/Image/schema.ts
+++ src/routers/Image/schema.ts
@@ -1,5 +1,6 @@
 import zod from "zod";
 import { ImageType } from "../../Enum/Enum";
+import { isValidId } from "../../tools/IdGenerator";
 export const post_img_schema = zod.object({
   id:zod
       .string({
@@ -17,7 +18,12 @@ export const post_img_schema = zod.object({
     image_ids:zod.array(zod.string({
       required_error: "ids not found in json",
       invalid_type_error: "ids need to be an ObjectID",
-    }),{invalid_type_error:'ids should be a string array',required_error:'ids is required in json file'})
+    }),{invalid_type_error:'ids should be a string array',required_error:'ids is required in json file'}).refine((arr)=>{
+      for(let i=0;i<arr.length;i++){
+        if(!isValidId(arr[i])&&arr[i]!=='NI') return false;
+      }
+      return true;
+    },'Element in image_ids only accept valid ID from server provided or "NI" which stand for new image')
   },
   { required_error: "Json file not found" }
 );
