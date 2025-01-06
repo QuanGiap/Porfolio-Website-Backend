923d97051ce00e58e628ffd6ff1ca5f846581b16
Quan Giap <minhquangiapnguyen@gmail.com>
2025-01-05 22:40:43 -0800

    add chai

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+import assert from 'assert';
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -6,3 +6,4 @@ describe('Array', function () {
     });
   });
 });
+
