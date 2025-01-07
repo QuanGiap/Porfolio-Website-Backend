d94da8ca78d200a114803ebda478bbc368a4fd4a
Quan Giap <minhquangiapnguyen@gmail.com>
2025-01-06 21:02:44 -0800

    Add layout for test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,40 @@
 import assert from 'assert';
-describe('Array', function () {
-  describe('#indexOf()', function () {
+import chaiHttp from 'chai-http';
+import chai from 'chai';
+import server from '../src/app'
+chai.use(chaiHttp)
+describe('Test all route API', function () {
+  describe('User route', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('Auth route', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('PortData route', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('Achievement route', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('Project route', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('Experience route', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+  describe('Image route', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1);
     });
